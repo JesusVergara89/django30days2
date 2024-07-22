5d5b449da5800a40ee1f84ee5f2642248abfc260
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-07-22 17:16:45 -0500

    new way to impot all urls to the project

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
-urlpatterns = [
-    #path('login/', views.user_login, name='login'),
+urlpatterns = [   
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+
+     #path('login/', views.user_login, name='login'),
+    """### this is replaced by only two lines of code
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password-change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
@@ -12,7 +16,8 @@ urlpatterns = [
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('password-reset/complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-    path('', views.dashboard, name='dashboard'),    
+    path('', views.dashboard, name='dashboard'),  
+    """
 ]
 
 
